0615c0a26a59752a12466a0b55e05eea478f7986
rakesh_verma <rkv160794@gmail.com>
2016-07-28 07:49:04 +0530

    extrating all data from scrapy

--- spidey.py
+++ spidey.py
@@ -1,4 +1,6 @@
-import scrapy
+mport scrapy
+
+from tutorial.items import DmozItem
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
@@ -9,6 +11,9 @@ class DmozSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
